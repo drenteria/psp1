1b590dc41cfc3fa28c77251b7f12008645fae1f3
Daniel Renteria <df.renteria10@uniandes.edu.co>
2015-02-09 01:55:27 -0500

    PSP1 v0.4: I/O por consola, inicio

---
+++ src/main/java/edu/uniandes/ecos/psp1/view/ConsoleApp.java
@@ -0,0 +1,23 @@
+package edu.uniandes.ecos.psp1.view;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class ConsoleApp {
+
+	public static void main(String[] args) {
+		System.out.println("--- Regression and Correlation Calculator ---");
+		System.out.println("Please, write the number of elements for each numeric set:");
+		
+		Scanner scanner = new Scanner(System.in);
+		Integer number = 0;
+		ArrayList<Double> setX = new ArrayList<Double>();
+		ArrayList<Double> setY = new ArrayList<Double>();
+		
+		number = Integer.valueOf(scanner.nextLine());
+		
+		
+		
+	}
+
+}
--- src/test/java/edu/uniandes/ecos/psp1/RegressionCorrelationCalculatorTest.java
+++ src/test/java/edu/uniandes/ecos/psp1/RegressionCorrelationCalculatorTest.java
@@ -1,12 +1,11 @@
 package edu.uniandes.ecos.psp1;
 
 import java.util.ArrayList;
-import java.util.LinkedList;
 
-import edu.uniandes.ecos.psp1.model.RegressionCorrelationCalculator;
 import junit.framework.Test;
 import junit.framework.TestCase;
 import junit.framework.TestSuite;
+import edu.uniandes.ecos.psp1.model.RegressionCorrelationCalculator;
 
 /**
  * Unit test for Regression and Correlation
@@ -63,18 +62,33 @@ public class RegressionCorrelationCalculatorTest
 	}
 	
 	private ArrayList<Double> loadActualAddedModifiedSize(){
-		ArrayList<Double> list02 = new ArrayList<Double>();
-		list02.add(186.0);
-		list02.add(699.0);
-		list02.add(132.0);
-		list02.add(272.0);
-		list02.add(291.0);
-		list02.add(331.0);
-		list02.add(199.0);
-		list02.add(1890.0);
-		list02.add(788.0);
-		list02.add(1601.0);
-		return list02;
+		ArrayList<Double> list03 = new ArrayList<Double>();
+		list03.add(186.0);
+		list03.add(699.0);
+		list03.add(132.0);
+		list03.add(272.0);
+		list03.add(291.0);
+		list03.add(331.0);
+		list03.add(199.0);
+		list03.add(1890.0);
+		list03.add(788.0);
+		list03.add(1601.0);
+		return list03;
+	}
+	
+	private ArrayList<Double> loadActualDevelopmentHours(){
+		ArrayList<Double> list04 = new ArrayList<Double>();
+		list04.add(15.0);
+		list04.add(69.9);
+		list04.add(6.5);
+		list04.add(22.4);
+		list04.add(28.4);
+		list04.add(65.9);
+		list04.add(19.4);
+		list04.add(198.7);
+		list04.add(38.8);
+		list04.add(138.2);
+		return list04;
 	}
 	
 	public void test01(){
@@ -93,12 +107,80 @@ public class RegressionCorrelationCalculatorTest
 		System.out.println("Xk: 386.0");
 		System.out.println("Yk: " + yk);
 		
-		assertEquals(1.7279, theCalc.getB1RegressionParameter(), 0.0001);
 		assertEquals(-22.55, theCalc.getB0RegressionParameter(), 0.01);
+		assertEquals(1.7279, theCalc.getB1RegressionParameter(), 0.0001);
 		assertEquals(0.9545, theCalc.getRXYCoeficcient(), 0.0001);
 		assertEquals(0.9111, theCalc.getRSquaredCoefficient(), 0.0001);
 		assertEquals(644.429, yk, 0.001);
+	}
+	
+	public void test02(){
+		System.out.println("Running Test Case 02");
+		theCalc.setInputSetX(loadEstimatedProxySize());
+		theCalc.setInputSetY(loadActualDevelopmentHours());
+		System.out.println("X Set: " + loadEstimatedProxySize().toString());
+		System.out.println("Y Set: " + loadActualDevelopmentHours().toString());
 		
+		double yk = theCalc.calculateImprovedPredictionYk(386.0);
+		
+		System.out.println("B0: " + theCalc.getB0RegressionParameter());
+		System.out.println("B1: " + theCalc.getB1RegressionParameter());
+		System.out.println("Rxy: " + theCalc.getRXYCoeficcient());
+		System.out.println("R2: " + theCalc.getRSquaredCoefficient());
+		System.out.println("Xk: 386.0");
+		System.out.println("Yk: " + yk);
+		
+		assertEquals(-4.039, theCalc.getB0RegressionParameter(), 0.001);
+		assertEquals(0.1681, theCalc.getB1RegressionParameter(), 0.0001);
+		assertEquals(0.9333, theCalc.getRXYCoeficcient(), 0.0001);
+		assertEquals(0.8711, theCalc.getRSquaredCoefficient(), 0.0001);
+		assertEquals(60.858, yk, 0.001);
+	}
+	
+	public void test03(){
+		System.out.println("Running Test Case 03");
+		theCalc.setInputSetX(loadPlanAddedModifiedSize());
+		theCalc.setInputSetY(loadActualAddedModifiedSize());
+		System.out.println("X Set: " + loadEstimatedProxySize().toString());
+		System.out.println("Y Set: " + loadActualAddedModifiedSize().toString());
+		
+		double yk = theCalc.calculateImprovedPredictionYk(386.0);
+		
+		System.out.println("B0: " + theCalc.getB0RegressionParameter());
+		System.out.println("B1: " + theCalc.getB1RegressionParameter());
+		System.out.println("Rxy: " + theCalc.getRXYCoeficcient());
+		System.out.println("R2: " + theCalc.getRSquaredCoefficient());
+		System.out.println("Xk: 386.0");
+		System.out.println("Yk: " + yk);
+		
+		assertEquals(-23.92, theCalc.getB0RegressionParameter(), 0.01);
+		assertEquals(1.43097, theCalc.getB1RegressionParameter(), 0.0001);
+		assertEquals(0.9631, theCalc.getRXYCoeficcient(), 0.0001);
+		assertEquals(0.9276, theCalc.getRSquaredCoefficient(), 0.0001);
+		assertEquals(528.4294, yk, 0.001);
+	}
+	
+	public void test04(){
+		System.out.println("Running Test Case 04");
+		theCalc.setInputSetX(loadPlanAddedModifiedSize());
+		theCalc.setInputSetY(loadActualDevelopmentHours());
+		System.out.println("X Set: " + loadEstimatedProxySize().toString());
+		System.out.println("Y Set: " + loadActualDevelopmentHours().toString());
+		
+		double yk = theCalc.calculateImprovedPredictionYk(386.0);
+		
+		System.out.println("B0: " + theCalc.getB0RegressionParameter());
+		System.out.println("B1: " + theCalc.getB1RegressionParameter());
+		System.out.println("Rxy: " + theCalc.getRXYCoeficcient());
+		System.out.println("R2: " + theCalc.getRSquaredCoefficient());
+		System.out.println("Xk: 386.0");
+		System.out.println("Yk: " + yk);
+		
+		assertEquals(-4.604, theCalc.getB0RegressionParameter(), 0.001);
+		assertEquals(0.140164, theCalc.getB1RegressionParameter(), 0.0001);
+		assertEquals(0.9480, theCalc.getRXYCoeficcient(), 0.0001);
+		assertEquals(0.8988, theCalc.getRSquaredCoefficient(), 0.0001);
+		assertEquals(49.4994, yk, 0.001);
 	}
 
 }
