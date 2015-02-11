7c05f2d50072b1cff021eb512b4fcdf6420221ec
Daniel Renteria <df.renteria10@uniandes.edu.co>
2015-02-11 06:25:54 -0500

    PSP1 v0.5: Web deploy.

---
+++ src/main/java/edu/uniandes/ecos/psp1/controller/WebController.java
@@ -0,0 +1,72 @@
+package edu.uniandes.ecos.psp1.controller;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.List;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import edu.uniandes.ecos.psp1.model.RegressionCorrelationCalculator;
+
+/**
+ * This class acts as the Controller for the Web view
+ * @author drenteria
+ *
+ */
+public class WebController {
+	
+	/**
+	 * Show main input form for the X and Y lists for regression
+	 * @param request
+	 * @param response
+	 * @throws IOException
+	 */
+	public static void showInputForm(HttpServletRequest request, HttpServletResponse response) 
+	throws IOException{
+		String formString = "<html>" +
+				"<body>" +
+				"<h1>PSP1 Regression and Correlation Program!</h1>" +
+				"<p>Please, write numbers to calculate in semicolon (;) separated values.</p>" +
+				"<p>Be sure both inputs have the same amount of numbers.</p>" +
+				"<form action=\"count\" method=\"post\">\n" +
+				"List X: <input type=\"text\" name=\"listX\">\n" +
+				"List Y: <input type=\"text\" name=\"listY\">\n" +
+				"Xk: <input type=\"text\" name=\"xk\">\n" +
+				"<input type=\"submit\" value=\"Show Improved Prediction (Yk)\">\n" +
+				"</body>" +
+				"</html>";
+		PrintWriter writer = response.getWriter();
+		writer.write(formString);
+	}
+	
+	/**
+	 * Show results 
+	 * @param request
+	 * @param response
+	 * @throws IOException
+	 */
+	public static void showRegressionCorrelationResponse(HttpServletRequest request, HttpServletResponse response, 
+			List<Double> listX, List<Double> listY, Double xk,
+			RegressionCorrelationCalculator theCalc)
+	throws IOException{
+		Double yk = theCalc.calculateImprovedPredictionYk(xk); 
+		String resultString = "<html>" +
+				"<body>" +
+				"<h1>PSP1 Regression and Correlation Program!</h1>" +
+				"<p>Results:</p>" +
+				"List X:" + listX.toString() + "\n" +
+				"List Y:" + listY.toString() + "\n" +
+				"B0: " + theCalc.getB0RegressionParameter() + 
+				"B1: " + theCalc.getB1RegressionParameter() +
+				"Rxy: " + theCalc.getRXYCoeficcient() + 
+				"R2: " + theCalc.getRSquaredCoefficient() +
+				"Xk:" + xk.toString() + "\n" +
+				"Yk:" + yk.toString() + "\n" +
+				"</body>" +
+				"</html>";
+		PrintWriter writer = response.getWriter();
+		writer.write(resultString);
+	}
+
+}
--- src/main/java/edu/uniandes/ecos/psp1/view/App.java
+++ src/main/java/edu/uniandes/ecos/psp1/view/App.java
@@ -1,13 +1,86 @@
 package edu.uniandes.ecos.psp1.view;
 
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.servlet.ServletContextHandler;
+import org.eclipse.jetty.servlet.ServletHolder;
+
+import edu.uniandes.ecos.psp1.controller.WebController;
+import edu.uniandes.ecos.psp1.model.RegressionCorrelationCalculator;
+
+
 /**
- * Hello world!
+ * Regression and Correlation Web Application
+ * @author drenteria
  *
  */
-public class App 
+public class App extends HttpServlet
 {
-    public static void main( String[] args )
+    private static final long serialVersionUID = 1L;
+
+	public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+    	Server server = new Server(Integer.valueOf(System.getenv("PORT")));
+        ServletContextHandler context = new ServletContextHandler(ServletContextHandler.SESSIONS);
+        context.setContextPath("/");
+        server.setHandler(context);
+        context.addServlet(new ServletHolder(new App()), "/*");
+        try {
+            server.start();
+            server.join();
+        } catch (Exception ex) {
+            Logger.getLogger(App.class.getName()).log(Level.SEVERE, null, ex);
+        }
     }
+	
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp){
+		try {
+			WebController.showInputForm(req, resp);
+		} catch (IOException e) {
+			Logger.getLogger(App.class.getName()).log(Level.SEVERE, null, e);
+			e.printStackTrace();
+		}
+	}
+	
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp){
+		String xValues = req.getParameter("listX");
+		String yValues = req.getParameter("listY");
+		String xkValue = req.getParameter("xk");
+		
+		try{
+			ArrayList<Double> listX = new ArrayList<Double>();
+			ArrayList<Double> listY = new ArrayList<Double>();
+			Double xk = Double.valueOf(xkValue);
+			
+			String[] xVals = xValues.split(";");
+			for(String xCurrentVal : xVals){
+				listX.add(Double.valueOf(xCurrentVal));
+			}
+			
+			String[] yVals = yValues.split(";");
+			for(String yCurrentVal : yVals){
+				listY.add(Double.valueOf(yCurrentVal));
+			}
+			
+			RegressionCorrelationCalculator theCalc = new RegressionCorrelationCalculator();
+			theCalc.setInputSetX(listX);
+			theCalc.setInputSetY(listY);
+			
+			WebController.showRegressionCorrelationResponse(req, resp, listX, listY, xk, theCalc);
+			
+		}
+		catch (Exception ex){
+			Logger.getLogger(App.class.getName()).log(Level.SEVERE, null, ex);
+			ex.printStackTrace();
+		}
+		
+	}
 }
